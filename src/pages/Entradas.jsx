4376713484adebfedf2cdff68e98fee1377787ce
Rangel Pinheiro <capette@capette.org>
2026-06-05 08:52:00 -0300

    Fix Entradas.jsx sintaxe quebrada causando crash

--- src/pages/Entradas.jsx
+++ src/pages/Entradas.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
 import Lancamentos from './Lancamentos'
+
 export default function Entradas() {
-  const isMobile = useIsMobile() return <Lancamentos tipo="entrada" /> }
+  return <Lancamentos tipo="entrada" />
+}
